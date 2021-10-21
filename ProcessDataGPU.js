4fe84fa1d5cbc5cc5e92f6810c69f93e3653c2c2
lenguyenkhoi21 <lenguyenkhoi21@gmail.com>
2021-10-21 13:28:45 +0700

    Finish writing gpu.csv

--- ProcessDataGPU.js
+++ ProcessDataGPU.js
@@ -5,12 +5,14 @@ const { writeToPath } = require('@fast-csv/format')
 
 const dataflow = new Promise((resolve, reject) => {
     const map = new Map()
-    const FACT_GPU_PRICE = './FACT_GPU_PRICE.csv'
+    const FACT_GPU_PRICE = 'FACT_GPU_PRICE.csv'
     const colId = `ProdId`
     const colPrice = `Price_Original`
     fs.createReadStream(path.resolve(__dirname, FACT_GPU_PRICE))
         .pipe(csv.parse({headers: true}))
         .on('error', error => {
+            console.log(`Error at processing file ${FACT_GPU_PRICE}`)
+            console.log(error)
             reject(error)
         })
         .on('data', row => {
@@ -25,12 +27,50 @@ const dataflow = new Promise((resolve, reject) => {
             }
         })
         .on('end', rowCount => {
-            console.log(`Parsed ${rowCount} rows`)
-            resolve({map})
+            console.log(`Parsed ${rowCount} rows in file ${FACT_GPU_PRICE}`)
+            resolve(map)
         })
 })
 
-const readGPUData = ({map}) => {
-    const GPU_PROD = 'DIM_GPU_PROD.csv'
-    const data = []
-}
+dataflow.then(map => {
+    const readGPUData = new Promise((resolve, reject) => {
+        const GPU_PROD = 'DIM_GPU_PROD.csv'
+        const Id = 'Id'
+        const data = []
+        const Price_Original = 'Price_Original'
+        let index = 0
+        fs.createReadStream(path.resolve(__dirname, GPU_PROD))
+            .pipe(csv.parse({headers: true}))
+            .on('error', error => {
+                console.log(`Error at processing file ${GPU_PROD}.csv`)
+                reject(error)
+            })
+            .on('data', row => {
+                row[Price_Original] = map.get(row[Id])
+                data.push(row)
+            })
+            .on('end', rowCount => {
+                console.log(`Parsed ${rowCount} rows in file ${GPU_PROD}`)
+                resolve(data)
+            })
+    })
+
+    const writeData = (data) => {
+        writeToPath(path.resolve(__dirname, 'gpu.csv'), data)
+            .on('error', err => console.error(err))
+            .on('finish', () => console.log('Done writing.'))
+    }
+
+    readGPUData
+        .then(writeData)
+        .catch(reason => {
+            console.log(reason)
+        })
+})
+    .catch(reason => {
+        console.log(reason)
+    })
+
+
+
+
