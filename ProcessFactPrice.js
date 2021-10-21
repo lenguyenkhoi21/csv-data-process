76e3b4ac90dca23142f1d438d95683f3ae7a7da8
lenguyenkhoi21 <lenguyenkhoi21@gmail.com>
2021-10-21 16:22:44 +0700

    convert to async await full

--- ProcessFactPrice.js
+++ ProcessFactPrice.js
@@ -3,40 +3,42 @@ const path = require('path')
 const csv = require('fast-csv')
 const { writeToPath } = require('@fast-csv/format')
 
-const dataflow = new Promise((resolve, reject) => {
-    const FACT_GPU_PRICE = 'FACT_GPU_PRICE.csv'
-    const Id = `Id`
-    const ProdId = `ProdId`
-    const TimeId = `TimeId`
-    const RegionId = `RegionId`
-    const MerchantId = `MerchantId`
-    const Price_USD = `Price_USD`
-    const headers = [Id, ProdId, TimeId, RegionId, MerchantId, Price_USD]
-    const data = []
-    data.push(headers)
-    let index = 0
-    fs.createReadStream(path.resolve(__dirname, FACT_GPU_PRICE))
-        .pipe(csv.parse({headers: true}))
-        .on('error', error => {
-            console.log(`Error at processing file ${FACT_GPU_PRICE}`)
-            console.log(error)
-            reject(error)
-        })
-        .on('data', row => {
-            const newRow = {}
-            newRow[Id] = ++index
-            newRow[ProdId] = row[ProdId]
-            newRow[TimeId] = row[TimeId]
-            newRow[RegionId] = row[RegionId]
-            newRow[MerchantId] = row[MerchantId]
-            newRow[Price_USD] = row[Price_USD]
-            data.push(newRow)
-        })
-        .on('end', rowCount => {
-            console.log(`Parsed ${rowCount} rows in file ${FACT_GPU_PRICE}`)
-            resolve(data)
-        })
-})
+const dataFactGPUPrice = () => {
+    return new Promise((resolve, reject) => {
+        const FACT_GPU_PRICE = 'FACT_GPU_PRICE.csv'
+        const Id = `Id`
+        const ProdId = `ProdId`
+        const TimeId = `TimeId`
+        const RegionId = `RegionId`
+        const MerchantId = `MerchantId`
+        const Price_USD = `Price_USD`
+        const headers = [Id, ProdId, TimeId, RegionId, MerchantId, Price_USD]
+        const data = []
+        data.push(headers)
+        let index = 0
+        fs.createReadStream(path.resolve(__dirname, FACT_GPU_PRICE))
+            .pipe(csv.parse({headers: true}))
+            .on('error', error => {
+                console.log(`Error at processing file ${FACT_GPU_PRICE}`)
+                console.log(error)
+                reject(error)
+            })
+            .on('data', row => {
+                const newRow = {}
+                newRow[Id] = ++index
+                newRow[ProdId] = row[ProdId]
+                newRow[TimeId] = row[TimeId]
+                newRow[RegionId] = row[RegionId]
+                newRow[MerchantId] = row[MerchantId]
+                newRow[Price_USD] = row[Price_USD]
+                data.push(newRow)
+            })
+            .on('end', rowCount => {
+                console.log(`Parsed ${rowCount} rows in file ${FACT_GPU_PRICE}`)
+                resolve(data)
+            })
+    })
+}
 
 const writeData = (data) => {
     writeToPath(path.resolve(__dirname, 'fact_price.csv'), data)
@@ -44,8 +46,11 @@ const writeData = (data) => {
         .on('finish', () => console.log('Done writing.'))
 }
 
-dataflow
-    .then(writeData)
-    .then(value => {
-        console.log(value)
-    })
+const dataflow = async () => {
+    const data = await dataFactGPUPrice()
+    writeData(data)
+}
+
+dataflow().then(()=>{})
+
+
--- ProcessMerchant.js
+++ ProcessMerchant.js
@@ -3,31 +3,33 @@ const path = require('path')
 const csv = require('fast-csv')
 const { writeToPath } = require('@fast-csv/format')
 
-const dataflow = new Promise((resolve, reject) => {
-    const DIM_MERCHANT = 'DIM_MERCHANT.csv'
-    const data = []
-    const Region_Id = `Region_Id`
-    const headers = [`Id`, `Merchant`, Region_Id]
-    data.push(headers)
-    const randomIntFromInterval = (min, max) => {
-        return Math.floor(Math.random() * (max - min + 1) + min)
-    }
-    fs.createReadStream(path.resolve(__dirname, DIM_MERCHANT))
-        .pipe(csv.parse({headers: true}))
-        .on('error', error => {
-            console.log(`Error at processing file ${DIM_MERCHANT}`)
-            console.log(error)
-            reject(error)
-        })
-        .on('data', row => {
-            row[Region_Id] = randomIntFromInterval(1,11)
-            data.push(row)
-        })
-        .on('end', rowCount => {
-            console.log(`Parsed ${rowCount} rows in file ${DIM_MERCHANT}`)
-            resolve(data)
-        })
-})
+const merchantData = () => {
+    return new Promise((resolve, reject) => {
+        const DIM_MERCHANT = 'DIM_MERCHANT.csv'
+        const data = []
+        const Region_Id = `Region_Id`
+        const headers = [`Id`, `Merchant`, Region_Id]
+        data.push(headers)
+        const randomIntFromInterval = (min, max) => {
+            return Math.floor(Math.random() * (max - min + 1) + min)
+        }
+        fs.createReadStream(path.resolve(__dirname, DIM_MERCHANT))
+            .pipe(csv.parse({headers: true}))
+            .on('error', error => {
+                console.log(`Error at processing file ${DIM_MERCHANT}`)
+                console.log(error)
+                reject(error)
+            })
+            .on('data', row => {
+                row[Region_Id] = randomIntFromInterval(1,11)
+                data.push(row)
+            })
+            .on('end', rowCount => {
+                console.log(`Parsed ${rowCount} rows in file ${DIM_MERCHANT}`)
+                resolve(data)
+            })
+    })
+}
 
 const writeData = (data) => {
     writeToPath(path.resolve(__dirname, 'merchant.csv'), data)
@@ -35,11 +37,13 @@ const writeData = (data) => {
         .on('finish', () => console.log('Done writing.'))
 }
 
-dataflow
-    .then(writeData)
-    .catch(reason => {
-        console.log(reason)
-    })
+const dataflow = async () => {
+    const data = await merchantData()
+    writeData(data)
+}
+
+dataflow().then(() => {})
+
 
 
 
